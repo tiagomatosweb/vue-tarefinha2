24fad8a69ece5869fdd7e3ebd0004cb380bae0ec
Tiago Matos <tiago@tiagomatos.com>
2025-11-20 13:42:02 +1100

    Calculando estatísticas de tarefas

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,24 @@
     <h1 class="text-center my-4">Tarefinha</h1>
     
     <!-- Stats -->
-    <!-- <div class="card mb-3">
+    <div class="card mb-3">
       <div class="card-body">
         <div class="row text-center">
           <div class="col-4">
-            <div class="fw-bold fs-4">3</div>
+            <div class="fw-bold fs-4">{{ totalTasks }}</div>
             <div class="text-muted small">Total</div>
           </div>
           <div class="col-4">
-            <div class="fw-bold fs-4 text-success">1</div>
+            <div class="fw-bold fs-4 text-success">{{ totalCompleted }}</div>
             <div class="text-muted small">Concluídas</div>
           </div>
           <div class="col-4">
-            <div class="fw-bold fs-4 text-warning">2</div>
+            <div class="fw-bold fs-4 text-warning">{{ totalPending }}</div>
             <div class="text-muted small">Pendentes</div>
           </div>
         </div>
       </div>
-    </div> -->
+    </div>
     
     <!-- Add new task -->
     <div class="input-group mb-3">
@@ -123,6 +123,9 @@ const filteredTasks = computed(() => {
 
   return output;
 });
+const totalTasks = computed(() => tasks.value.length)
+const totalCompleted = computed(() => tasks.value.filter(o => o.completed).length)
+const totalPending = computed(() => tasks.value.filter(o => !o.completed).length)
 
 const clearFilters = () => {
   filterSearch.value = ''
