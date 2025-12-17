ed9672386af7473aa070459fd2c7ba8e355a0149
Tiago Matos <tiago@tiagomatos.com>
2025-12-18 10:53:27 +1100

    Commit bla!

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container" style="max-width: 800px;">
-    <h1 class="text-center my-4">Tarefinha 3</h1>
+    <h1 class="text-center my-4">Tarefinha</h1>
     
     <!-- Stats -->
     <TaskStats :tasks="tasks" />
